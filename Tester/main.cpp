a96bd0bb20242955a5c9bb7eb3045dc550fbf001
Tyler Stein <tyler.stein1@yahoo.ca>
2016-10-08 12:10:24 -0400

    Updated GitIgnore
    Gitattributes now ignore unecessary visual studio temp files! Also
    removed some unecessary comments from Tester.cpp

--- Tester/main.cpp
+++ Tester/main.cpp
@@ -1,25 +1,6 @@
 #pragma once
 #include "PhageEngine.h"
 
-/*
-//Placeholder hardcoded shaders
-const char* vertex_shader =
-"#version 400\n"
-"in vec3 vp;"
-"void main() {"
-"  gl_Position = vec4(vp, 1.0);"
-"}";
-
-const char* fragment_shader =
-"#version 400\n"
-"out vec4 frag_colour;"
-"void main() {"
-"  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
-"}";
-
-//Create some points to render
-
-*/
 
 int main() {
 	//Create the phage engine on the heap 
