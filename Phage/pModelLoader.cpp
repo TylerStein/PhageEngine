26f6349de6c3284f22d71853b6e7b1de24deadff
Kojah <podkolzin@live.ca>
2016-10-22 12:16:01 -0400

    Complete Model Loader
    Loads a model, stores its uv, position, and normal vertices in glm vec3
    vectors, has getters to retrieve the storage vectors.

--- Phage/pModelLoader.cpp
+++ Phage/pModelLoader.cpp
@@ -1,12 +1,69 @@
 #include "pModelLoader.h"
 
-
-
-pModelLoader::pModelLoader()
+pModelLoader::pModelLoader(GLchar * path)
 {
+	this->loadModel(path);
+}
+
+void pModelLoader::loadModel(std::string path)
+{
+	Assimp::Importer importer;
+	const aiScene* scene = importer.ReadFile(path,
+		aiProcess_CalcTangentSpace |
+		aiProcess_Triangulate |
+		aiProcess_JoinIdenticalVertices |
+		aiProcess_SortByPType);
+
+	for (int i; i < scene->mNumMeshes; i++)
+	{
+		aiMesh* mesh = scene->mMeshes[i];
+		int iMeshFaces = mesh->mNumFaces;
+		for (int j; j < iMeshFaces; j++)
+		{
+			const aiFace& face = mesh->mFaces[j];
+			for (int k; k < 3; k++)
+			{
+				aiVector3D pos = mesh->mVertices[face.mIndices[k]];
+				aiVector3D uv = mesh->mTextureCoords[0][face.mIndices[k]];
+				aiVector3D normal = mesh->HasNormals() ? mesh->mNormals[face.mIndices[k]] : aiVector3D(1.0f, 1.0f, 1.0f);
+
+				glm::vec3 posConversion;
+				glm::vec3 uvConversion;
+				glm::vec3 normConversion;
+
+				posConversion.x = pos.x;
+				posConversion.y = pos.y;
+				posConversion.z = pos.z;
+				uvConversion.x = uv.x;
+				uvConversion.y = uv.y;
+				uvConversion.z = uv.z;
+				normConversion.x = normal.x;
+				normConversion.y = normal.y;
+				normConversion.z = normal.z;
+
+
+				positions->push_back(posConversion);
+				uvs->push_back(uvConversion);
+				normals->push_back(normConversion);
+			}
+		}
+	}
+}
+
+std::vector<glm::vec3> pModelLoader::getPositions()
+{
+	return *positions;
+}
+
+std::vector<glm::vec3> pModelLoader::getUVs()
+{
+	return *uvs;
+}
+
+std::vector<glm::vec3> pModelLoader::getNormals()
+{
+	return *normals;
 }
 
 
-pModelLoader::~pModelLoader()
-{
-}
+
--- Phage/pModelLoader.h
+++ Phage/pModelLoader.h
@@ -1,10 +1,46 @@
 #pragma once
-#include "assimp\Importer.hpp"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <GL\glew.h>
+#include <glm\glm.hpp>
+#include <vector>
+#include <string>
 
 class pModelLoader
 {
 public:
-	pModelLoader();
-	~pModelLoader();
+	/*Constructor
+	  Takes in the path to the model, uses it to
+	  call @func loadModel*/
+	pModelLoader(GLchar* path);
+
+	/*Getters:
+	  @func getPositions returns the vector containing position vertices
+	  @func getUVs returns the vector containing UV data
+	  @func getNormals returns the vector containing normal data*/
+	std::vector<glm::vec3> getPositions();
+	std::vector<glm::vec3> getUVs();
+	std::vector<glm::vec3> getNormals();
+
+private:
+	/*Function uses provided path to load a model.
+	  first it creates an Assimp Importer object,
+	  uses it to create a scene containing the model to
+	  be imported. Then a tri-layer loop begins.
+	  In this loop, the data of every face on every mesh
+	  belonging to the model is retrieved, converted
+	  from aiVector3D types to glm::vec3s and 
+	  stored in their respective vectors.*/
+	void loadModel(std::string path);
+
+	/*Storage Vectors
+	  @var positions stores model position vertices
+	  @var uvs stores the model uv vectors
+	  @var normals stores the model normal vectors*/
+	std::vector<glm::vec3> *positions;
+	std::vector<glm::vec3> *uvs;
+	std::vector<glm::vec3> *normals;
+
 };
 
